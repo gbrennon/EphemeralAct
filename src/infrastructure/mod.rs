685d3d49528092bbbeeea8165f40a1474552c2c5
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-03 03:13:32 -0300

    style(infrastructure): fix formatting of re-exports

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,5 +1,5 @@
-pub mod actions_executor;
 pub mod act_wrappers;
+pub mod actions_executor;
 
-pub use actions_executor::ActionsExecutor;
 pub use act_wrappers::github_act_wrapper::GitHubActWrapper;
+pub use actions_executor::ActionsExecutor;
