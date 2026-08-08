0fc2be940e9b76476d7508bfd583e7231981f225
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-08 00:58:19 -0300

    feat(infra): register new infrastructure modules

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,10 +1,9 @@
-pub mod act_wrappers;
-pub mod actions_executor;
-
-pub use act_wrappers::github_act_wrapper::GitHubActWrapper;
-pub use actions_executor::ActionsExecutor;
 pub mod container;
+pub mod in_memory_event_bus;
+pub mod platform_image_mapper;
 pub mod runners;
 
 pub use container::Container;
+pub use in_memory_event_bus::InMemoryEventBus;
+pub use platform_image_mapper::PlatformImageMapper;
 pub use runners::ContainerRuntimeAdapter;
