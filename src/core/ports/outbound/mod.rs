1641598c8d482ca71821f9c77e2f0029b10c52fe
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-12 19:55:33 -0300

    refactor(outbound): remove duplicate container error and use core errors

--- src/core/ports/outbound/mod.rs
+++ src/core/ports/outbound/mod.rs
@@ -1,5 +1,5 @@
 pub mod container_config;
-pub mod container_error;
+
 pub mod container_runtime;
 pub mod event_publisher;
 pub mod exec_result;
@@ -8,7 +8,6 @@ pub mod host_info;
 pub mod image_mapper;
 pub mod runner_context;
 pub use container_config::ContainerConfig;
-pub use container_error::ContainerError;
 pub use container_runtime::{ContainerPort, ContainerRuntimePort};
 pub use event_publisher::EventPublisherPort;
 pub use exec_result::ExecResult;
@@ -16,3 +15,5 @@ pub use file_entry::FileEntry;
 pub use host_info::HostInfo;
 pub use image_mapper::ImageMapperPort;
 pub use runner_context::RunnerContext;
+
+pub use crate::core::errors::ContainerError;
