ff2ef71a503a1d699f272169601760abfd84d25d
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-12 19:55:10 -0300

    refactor(expression): remove duplicate error definitions and use core errors

--- src/core/expression/mod.rs
+++ src/core/expression/mod.rs
@@ -1,27 +1,23 @@
 pub mod compare_op;
 pub mod context;
-pub mod eval_error;
 pub mod evaluator;
 pub mod expr;
 pub mod functions;
 pub mod lexer;
-pub mod lexer_error;
 pub mod literal;
 pub mod logical_op;
-pub mod parse_error;
 pub mod parser;
 pub mod token;
 
 pub use compare_op::CompareOp;
 pub use context::EvalContext;
-pub use eval_error::EvalError;
 pub use evaluator::Evaluator;
 pub use expr::Expr;
 pub use functions::Functions;
 pub use lexer::Lexer;
-pub use lexer_error::LexerError;
 pub use literal::Literal;
 pub use logical_op::LogicalOp;
-pub use parse_error::ParseError;
 pub use parser::Parser;
 pub use token::Token;
+
+pub use crate::core::errors::{EvalError, LexerError, ParseError};
