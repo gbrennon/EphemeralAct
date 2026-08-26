705ce6034e14258a55bc36dd223513835df89f8c
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-26 01:57:57 -0300

    test(cli): handle list actions with fake port

---
+++ tests/presentation/cli/list_actions_handler_tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use crate::common::fakes::fake_list_actions_port::FakeListActionsPort;
+    use ephemeral_act::presentation::cli::{
+        ListActionsHandler, parse_list_actions_test_args,
+    };
+
+    #[test]
+    fn handle_with_fake_port_succeeds_on_empty_response() {
+        let args = parse_list_actions_test_args(&[]);
+        let port = FakeListActionsPort::new();
+
+        let result = ListActionsHandler::handle(args, &port);
+
+        assert!(result.is_ok());
+    }
+}
