b895fc33b068a5d316cf7c1766b4d2abc05f6e39
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-26 01:57:57 -0300

    test(cli): handle list workflows with fake port

---
+++ tests/presentation/cli/list_workflows_handler_tests.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use crate::common::fakes::fake_list_workflows_port::FakeListWorkflowsPort;
+    use ephemeral_act::presentation::cli::{
+        ListWorkflowsHandler, parse_list_workflows_test_args,
+    };
+
+    #[test]
+    fn handle_with_fake_port_succeeds_on_empty_response() {
+        let args = parse_list_workflows_test_args(&[]);
+        let port = FakeListWorkflowsPort::new();
+
+        let result = ListWorkflowsHandler::handle(args, &port);
+
+        assert!(result.is_ok());
+    }
+}
