8213510bb70e31ea06ac84f8c24e0536ab71966b
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-26 01:55:27 -0300

    feat(infrastructure): export workflow parser and AppContainer

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -4,8 +4,10 @@ pub mod container;
 pub mod in_memory_event_bus;
 pub mod platform_image_mapper;
 pub mod runners;
+pub mod workflow_file_parser;
 
-pub use container::Container;
+pub use container::{AppContainer, Container};
 pub use in_memory_event_bus::InMemoryEventBus;
 pub use platform_image_mapper::PlatformImageMapper;
 pub use runners::ContainerRuntimeAdapter;
+pub use workflow_file_parser::FilesystemWorkflowFileParser;
